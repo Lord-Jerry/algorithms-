f4362157c48ab4db82c8fb0e083b31339e85c0ca
Lord Jerry <lovinjerry004@gmail.com>
2021-01-24 10:22:59 +0100

    refactor javascript binary search implementation.

--- Javascript/binary.js
+++ Javascript/binary.js
@@ -1,41 +1,31 @@
+let steps = 0;
 /**
- * * #### NOTE :
- * * Implimentation of binary search in javascript
- * * #### TODO :
- * * should make it work with alphabets
- * @param { Array } - an array of elements to search through
- * @param { Number } - element to be found in the array
+ * @param { Array<Integer | String>} - an array of elements to search through
+ * @param { Integer | String } - element to be found in the array
  */
 const binary = (arr, item) => {
-  count = count + 1;
-  // get the average of the array's length
-  const middle = Math.floor((arr.length)/2);
-  let temp;
+  // increment step counter
+  steps += 1;
+  if (arr.length === 1 && item !== arr[0]) return "not found";
 
-  // if item is not found
-  if (arr.length === 1 && item !== arr[0]) return 'not found';
-  
-  // if item is found in the array 
-  if (item === arr[middle]) return 'found';
-  
-  // check if the middle element in the array, 
-  // is lesser than the item we are searching for
-  // if yes, assign to ``temp`` from d middle of the array to the end 
-  else if (arr[middle] < item) {
-    temp = arr.slice(middle, arr.length);
+  const middle = Math.floor(arr.length / 2);
 
-  }else if (arr[middle] > item) {
-    temp = arr.slice(0, middle);
-  }
+  if (item === arr[middle]) return "found";
+  if (arr[middle] < item) return binary(arr.slice(middle, arr.length) , item);
+  if (arr[middle] > item) return binary(arr.slice(0, middle), item);
 
-  //recursively call itself with a new array ``temp``
-  return binary(temp, item);
-}
+  return;
+};
+
 // tests
-let count = 0;
 let arr1 = [];
-let temp =0;
-while (arr1.length < 24763543) {temp++; arr1.push(temp);}
-console.time('search');
-console.log(`${binary(arr1, 678964)} in ${count} steps`);
-console.timeEnd('search');
+let temp = 0;
+while (arr1.length < 24763543) {
+  temp++;
+  arr1.push(temp);
+}
+console.time("search");
+console.log(`${binary(arr1, 678964)} in ${steps} steps`);
+console.timeEnd("search");
+
+
